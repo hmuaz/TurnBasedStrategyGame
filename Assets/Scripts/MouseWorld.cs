8bf14dd65c321f57be1f3b0b2d7f55b6bbe0961b
hmuaz <muazsenol1@gmail.com>
2023-10-23 13:58:09 +0300

    Mouse Raycast

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(Physics.Raycast(ray));
+
+    }
+}
